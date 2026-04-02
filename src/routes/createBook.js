9bf90ccd18397914cb000f57a3ac74eb68d41029
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2026-04-02 16:20:38 +0530

    fix the path issue in createBook

--- src/routes/createBook.js
+++ src/routes/createBook.js
@@ -1,12 +1,5 @@
 import express from 'express';
 
-// import { PrismaClient } from '@prisma/client';
-// import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-// const adapter = new PrismaBetterSqlite3({
-//   url: 'file:./dev.db',
-// });
-
-// export const prisma = new PrismaClient({ adapter });
 import path from 'path';
 
 import { PrismaClient } from '@prisma/client';
