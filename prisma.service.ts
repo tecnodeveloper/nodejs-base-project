2704dc3bcd98e685e1c814178c95eef384bc0467
Zain Ali <recluzedev@gmail.com>
2026-05-01 21:32:29 +0500

    Intial commit

--- prisma.service.ts
+++
@@ -1,8 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
-
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
-}
